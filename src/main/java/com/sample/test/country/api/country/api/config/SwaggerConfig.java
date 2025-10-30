2080095b83449bf653854389c68e2f895922bbe9
Pavan Fernando <pavanfernando6@gmail.com>
2025-10-30 15:47:59 +0530

    Added swagger doc

---
+++ src/main/java/com/sample/test/country/api/country/api/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.sample.test.country.api.country.api.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI taskManagerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Country APIs")
+                        .description("test - Pavan Fernando")
+                        .version("v1.0"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Country APIs"));
+    }
+}
